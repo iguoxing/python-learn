a3037dbb78a436005cf77c9ea9699acb6a42bc73
xinghuotianci <iguoxing@163.com>
2020-06-12 21:30:13 +0800

    Day003
    Python运行方式

---
+++ 003环境搭建.py
@@ -0,0 +1,16 @@
+print('Python官网：https://www.python.org/')
+
+
+# Python文档下载地址：https://www.python.org/doc/
+
+
+
+# 环境变量配置
+#
+# 运行Python
+#
+# 1、交互式解释器：
+#
+# 2、命令行脚本
+
+# 3、集成开发环境（IDE：Integrated Development Environment）: PyCharm
