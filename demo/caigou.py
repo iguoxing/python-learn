ae235bc0fc0239b16f4bb2b7a8d1b762105a1818
xinghuotianci <iguoxing@163.com>
2020-08-20 09:39:56 +0800

    招标信息，解析网页数据

--- demo/caigou.py
+++ demo/caigou.py
@@ -11,6 +11,7 @@ keyword='实训平台'
 print(keyword)
 keyword=urllib.parse.quote(keyword)
 print(keyword)
+
 url='http://search.ccgp.gov.cn/bxsearch?searchtype=1&page_index=1' \
     '&start_time=&end_time=&timeType=2&searchparam=&searchchannel=0&dbselect=bidx&' \
     'kw='+keyword+'&bidSort=0&pinMu=0&bidType=0&buyerName=&projectId=&displayZone=&zoneId=&agentName=';
@@ -21,12 +22,16 @@ kv = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.3
 
 req = requests.get(url,headers = kv)
 result = req.content.decode('utf-8')
-# print(req)
-# print('-------------------------------')
-print(result)
 
 # 再次封装，获取具体标签内的内容
 bs = BeautifulSoup(result,'html.parser')
 # # 具体标签
-print("解析后的数据")
-print(bs.span)
+# print("解析后的数据")
+# print(bs.span)
+
+# 获取已爬取内容中的td标签内容
+data=bs.find_all('a')
+print(data)
+# 循环打印输出
+for i in data:
+    print(i.text)
